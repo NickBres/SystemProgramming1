0cb260949ee591bee5a7d70eff26b5857e290128
NickBres <breslavsky97@gmail.com>
2022-11-13 17:26:58 +0200

    scanf without text

--- main.c
+++ main.c
@@ -4,9 +4,7 @@
 int main(){
     int num1,num2,i;
 
-    printf("Enter first number: ");
     scanf("%d",&num1);
-    printf("Enter second number: ");
     scanf("%d",&num2);
 
     printf("The Armstrong numbers are: ");
